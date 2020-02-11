c7ea7d6705cc5a93f016c035f3f31bf94b72a668
UncleScrooge2012 <naruk2024@gmail.com>
2020-02-12 00:03:56 +0300

    add section overview and works and features and screenshots

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,29 @@
 $(function(){
 
+var mySwiper = new Swiper ('.swiper-container', {
+  // Optional parameters  
+  loop: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+  // Navigation arrows
+  
+})
+
+var mySwiper = new Swiper ('.people__swiper', {
+  // Optional parameters  
+  loop: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+  // Navigation arrows
+  
+})
+
 
   
 });
